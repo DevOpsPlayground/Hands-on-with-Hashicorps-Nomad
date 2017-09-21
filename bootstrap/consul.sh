8ecb2c684d5259f092d9e4024de89db8356ec71f
Daniel M <danielm@ECS0483.local>
2017-09-21 13:59:21 +0100

    update the scripts

--- bootstrap/consul.sh
+++ bootstrap/consul.sh
@@ -4,8 +4,3 @@ yum install -y docker
 usermod -a -G docker ec2-user
 service docker start
 chkconfig docker on
-
-curl -O https://releases.hashicorp.com/consul/0.8.5/consul_0.8.5_linux_amd64.zip
-unzip consul_0.8.5_linux_amd64.zip
-rm -f consul_0.8.5_linux_amd64.zip
-mv consul /usr/local/bin
