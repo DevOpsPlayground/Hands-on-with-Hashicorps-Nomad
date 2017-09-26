2a649b7822e3e725d3c1ef88a173a3b92a3527ac
Daniel M <danielm@ECS0483.local>
2017-09-26 11:59:06 +0100

    updated bootstrap script

--- bootstrap/nomad.sh
+++ bootstrap/nomad.sh
@@ -8,6 +8,17 @@ chkconfig docker on
 curl -O https://releases.hashicorp.com/nomad/0.6.3/nomad_0.6.3_linux_amd64.zip
 unzip nomad_0.6.3_linux_amd64.zip
 rm -f nomad_0.6.3_linux_amd64.zip
-mv nomad /usr/local/bin
+sudo mv nomad /usr/local/bin
 
 sudo sed -i -e "s|/sbin\:/bin\:/usr/sbin\:/usr/bin|/sbin\:/bin\:/usr/sbin\:/usr/bin\:/usr/local/bin|g" /etc/sudoers
+
+
+aws s3 sync s3://devops-playground-configurations/ /home/ec2-user
+
+sudo chmod 777 /home/ec2-user/client.hcl
+sudo chmod 777 /home/ec2-user/server.hcl
+
+sudo sed -i "s/CLIENT_INTERNAL_IP/$(ifconfig eth0 | awk '/inet addr/ { print $2}' | sed 's#addr:##g')/g" /home/ec2-user/client.hcl
+
+sudo sed -i "s/SERVER_INTERNAL_IP/$(ifconfig eth0 | awk '/inet addr/ { print $2}' | sed 's#addr:##g')/g" /home/ec2-user/server.hcl
+
